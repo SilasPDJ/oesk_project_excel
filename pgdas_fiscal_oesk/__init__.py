b3bcd9fec0217cf6669b7f7fb095507655252cd8
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2022-02-21 23:02:45 -0300

    Update __init__.py
    pgdas_fiscal_oesk init update

--- pgdas_fiscal_oesk/__init__.py
+++ pgdas_fiscal_oesk/__init__.py
@@ -11,21 +11,22 @@ class Consultar(Initial):
         self.MAIN_FILE = self.getset_folderspath(False)
         self.ATUAL_COMPT = get_compt(m_cont=-1) if compt is None else compt
 
-        self.DADOS_PADRAO = pd.read_excel(
-            self.MAIN_FILE, sheet_name='DADOS_PADRÃO', dtype=str).to_dict()
-# .to_html
-        self.DADOS = self.__lsdv(self.DADOS_PADRAO)
+        self.__DADOS_PADRAO = pd.read_excel(
+            self.MAIN_FILE, sheet_name='DADOS_PADRÃO', dtype=str)
+        self.DADOS_compt_atual = pd.read_excel(
+            self.MAIN_FILE, sheet_name=self.ATUAL_COMPT)
 
     def __lsdv(self, dta):
         # list of dict values = lsdv meaning
         return list(dta.values())
 
     def consultar_geral(self):
+        DADOS_PADRAO = self.__lsdv(self.__DADOS_PADRAO.to_dict())
         cont = 0
         while True:
             try:
                 razao_social, cnpj, cpf, codigo_simples, imposto_a_calcular, email, gissonline, giss_login, ginfess_cod, ginfess_link, dividas_ativas, proc_ecac = [
-                    list(d.values())[cont] for d in self.DADOS]
+                    list(d.values())[cont] for d in DADOS_PADRAO]
 
                 if str(razao_social) == 'nan':
                     break
@@ -34,11 +35,15 @@ class Consultar(Initial):
             except IndexError:
                 break
 
+    def get_fieldnames(self):
+        CAMPOS_COMPT_ATUAL = list(self.DADOS_compt_atual.to_dict().keys())
+        returned = list(self.__DADOS_PADRAO.to_dict().keys()) + \
+            CAMPOS_COMPT_ATUAL
+        return sorted(set(returned))  # list
+
     def consultar_compt(self):
 
-        compt_atual = pd.read_excel(
-            self.MAIN_FILE, sheet_name=self.ATUAL_COMPT)
-        df = self.__lsdv(compt_atual.to_dict())
+        df = self.__lsdv(self.DADOS_compt_atual.to_dict())
 
         cont = 0
         while True:
