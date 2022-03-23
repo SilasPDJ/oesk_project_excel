3a8b64615c65ecb6053aab3e34571f80f45eb34b
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2022-03-23 10:39:11 -0300

    Update oesk/init refactor

--- pgdas_fiscal_oesk/__init__.py
+++ pgdas_fiscal_oesk/__init__.py
@@ -16,30 +16,26 @@ class Consultar(Initial):
         self.DADOS_compt_atual = pd.read_excel(
             self.MAIN_FILE, sheet_name=self.ATUAL_COMPT)
 
-    def __lsdv(self, dta):
-        # list of dict values = lsdv meaning
-        return list(dta.values())
-
     def consultar_geral(self):
         DADOS_PADRAO = self.__lsdv(self.__DADOS_PADRAO.to_dict())
         cont = 0
         while True:
             try:
-                razao_social, cnpj, cpf, codigo_simples, imposto_a_calcular, email, gissonline, giss_login, ginfess_cod, ginfess_link, dividas_ativas, proc_ecac = [
+                once = razao_social, cnpj, cpf, codigo_simples, imposto_a_calcular, email, gissonline, giss_login, ginfess_cod, ginfess_link, dividas_ativas, proc_ecac = [
                     list(d.values())[cont] for d in DADOS_PADRAO]
 
                 if str(razao_social) == 'nan':
                     break
-                yield razao_social, cnpj, cpf, codigo_simples, imposto_a_calcular, email, gissonline, giss_login, ginfess_cod, ginfess_link, dividas_ativas, proc_ecac
+                yield once
                 cont += 1
             except IndexError:
                 break
 
     def get_fieldnames(self):
-        CAMPOS_COMPT_ATUAL = list(self.DADOS_compt_atual.to_dict().keys())
-        returned = list(self.__DADOS_PADRAO.to_dict().keys()) + \
-            CAMPOS_COMPT_ATUAL
-        return returned  # list
+        tutti = list(self.DADOS_compt_atual.to_dict().keys())
+        tutti += [f for f in list(self.__DADOS_PADRAO.to_dict().keys())
+                  if f not in tutti]
+        return tutti
 
     def consultar_compt(self):
 
@@ -56,3 +52,15 @@ class Consultar(Initial):
             except IndexError:
                 break
             cont += 1
+
+    def __lsdv(self, dta):
+        # list of dict values = lsdv meaning
+        return list(dta.values())
+
+    def __read_pandas(self, shname):
+        import pandas as pd
+        filename = self.MAIN_FILE
+
+        main_pandas = pd.read_excel(
+            filename, sheet_name=shname or shname.lower())
+        return main_pandas
