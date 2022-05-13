9a8e76a3a2e401c6830c9d5bd0518d96f823d29a
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2022-05-12 22:02:18 -0300

    Create g5_por_partes.py

---
+++ pgdas_fiscal_oesk/g5_por_partes.py
@@ -0,0 +1,6 @@
+# faz entradas após filtar
+# faz ISS depois de baixar
+# faz saídas depois de ter na pasta
+# G5 POR PARTES, POR PARTES NAO MAIS TUDO DE UMA VEZ
+# O MAIS FODA É AS SAÍDAS
+# EU PRECISO ORAR
