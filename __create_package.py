245377e039bbde0a638d1d56512f7ab7988854e1
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2022-11-02 16:29:46 -0300

    main scripts update

--- __create_package.py
+++ __create_package.py
@@ -4,6 +4,8 @@ import sys
 
 # https://pt.stackoverflow.com/questions/220078/o-que-significa-o-erro-execu%C3%A7%C3%A3o-de-scripts-foi-desabilitada-neste-sistema
 package = sys.argv[1]
-
-os.mkdir(package)
+try:
+    os.mkdir(package)
+except FileExistsError:
+    pass
 open(os.path.join(package, '__init__.py'), 'w').close()
