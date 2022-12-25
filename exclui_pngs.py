5a600cb8964e1fefede349928c6efee41218bd41
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2022-12-25 17:15:15 -0300

    Update exclui_pngs.py

--- exclui_pngs.py
+++ exclui_pngs.py
@@ -1,10 +1,12 @@
 import pathlib
 from __backend import COMPT, main_folder, main_file
+from default.sets import InitialSetting
 
 import os
+
+
 class ChangePng:
     def __init__(self, searched, option, path=os.path.join(main_folder, '..'), ext='.png'):
-
 
         for dirpath, dirnames, filenames in os.walk(path):
             for dirname in dirnames:
@@ -14,6 +16,7 @@ class ChangePng:
             for filename in filenames:
                 if searched in filename:
                     # print(dirpath)
+
                     # print(filename)
                     changed = os.path.join(dirpath, filename)
                     if option == 0:
@@ -22,14 +25,19 @@ class ChangePng:
                         except FileNotFoundError:
                             pass
                     elif option == 1:
-                        pass
-                        # png2pdf
+                        # COM ESSA CONVERSÃO, será anexado para o e-mail...
+                        try:
+                            pdf_filename = os.path.splitext(filename)[0]+'.pdf'
+                            InitialSetting.convert_img2pdf(
+                                filename, pdf_filename, dirpath)
+                            # png2pdf
+                            print(dirpath)
+                        except:
+                            pass
+                        # TODO: ceck-up da quantidade de anexos para envio 2 ou 4?? Pois agora incrementou
+
+# try to always use F5
+# ChangePng('SimplesNacional', 0)
 
 
-        # try to always use F5
-
-from default.sets import InitialSetting
-
-ChangePng('SimplesNacional', 0)
-
-ChangePng('ginfessDone')
+ChangePng('ginfessDone', 1)
